e305e819bc7e702777dfbf63791e33c1f2242e62
greedy <greedy@163.com>
2016-08-20 15:24:26 +0800

    weixin

---
+++ js/tbw.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/8/2.
+ */
+function fontAuto(width){
+    var winW=document.documentElement.clientWidth;
+    if(winW>=width){
+        document.documentElement.style.fontSize="625%";
+    }else{
+        document.documentElement.style.fontSize=(winW/width*625)+"%";
+    }
+}
+fontAuto(750);
+window.onresize=function(){
+    fontAuto(750);
+}
